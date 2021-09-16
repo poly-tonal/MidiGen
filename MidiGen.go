d8cf1fb1237b01ad19571f9762525c38abaf5e99
ellie <ellieepprice@gmail.com>
2021-09-16 01:23:34 +0100

    added note clamping, minor key, and octave randomisation

--- MidiGen.go
+++ MidiGen.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"encoding/binary"
+	"errors"
 	"fmt"
 	"math/rand"
 	"os"
@@ -15,17 +15,49 @@ import (
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	var (
-		input    string
+		noteIn   string
 		numNotes int
-		Rerr     error
+		keyIn    string
+		key      int
+		octIn    string
+		oct      int
+		noteRerr error
+		octRerr  error
 	)
+
 	//number of notes to generate
 	fmt.Printf("How many notes would you like to generate?\n")
-	fmt.Scanln(&input)
-	if input == "0" || input == "" {
-		numNotes = rand.Intn(10)
+	fmt.Scanln(&noteIn)
+	if noteIn == "0" || noteIn == "" {
+		//random number of notes
+		numNotes = rand.Intn(64)
 	} else {
-		numNotes, Rerr = strconv.Atoi(input)
+		numNotes, noteRerr = strconv.Atoi(noteIn)
+	}
+	if noteRerr != nil {
+		fmt.Printf("could not read input %v\n", noteIn)
+		os.Exit(1)
+	}
+
+	//select major or minor
+	fmt.Printf("major or minor? (0 for major, 1 for minor) default is major\n")
+	fmt.Scanln(&keyIn)
+	if keyIn == "1" || keyIn == "minor" || keyIn == "min" {
+		key = 1
+	} else {
+		key = 0
+	}
+
+	//octaves to span (default is 1)
+	fmt.Printf("how many octaves would you like to span?\n")
+	fmt.Scanln(&octIn)
+	oct, octRerr = strconv.Atoi(octIn)
+	if oct == 0 {
+		octRerr = errors.New("Value: value cannot be 0")
+	}
+	if octRerr != nil {
+		fmt.Printf("could not read input %v\n", octIn)
+		os.Exit(2)
 	}
 
 	//check for /midi dir in working dir to save .mid to
@@ -43,22 +75,23 @@ func main() {
 	name := "randMidi_" + strconv.Itoa(time.Now().Hour()) + strconv.Itoa(time.Now().Minute()) + strconv.Itoa(time.Now().Second()) + ".mid"
 	f := filepath.Join(dir, name)
 
+	//write midi file
 	err := writer.WriteSMF(f, 1, func(wr *writer.SMF) error {
 		wr.SetChannel(0)
 		for n := 0; n < numNotes; n++ {
-			var note uint8
-			noteTemp := rand.Uint32()
-			temp := make([]byte, 4)
-			binary.LittleEndian.PutUint32(temp, noteTemp)
-
-			note = temp[3]
-			//TODO Clamp values to normal range
-			//add key filters
+			//TODO
 			//make it play notes
+			//randomise SetDeltas for different length of notes
+
+			note := note(key, oct)
 
 			writer.NoteOn(wr, note, 50)
-			wr.SetDelta(120)
+			//length of note on
+			wr.SetDelta(240)
+
 			writer.NoteOff(wr, note)
+			//length of note off
+			wr.SetDelta(120)
 		}
 		writer.EndOfTrack(wr)
 
@@ -70,11 +103,82 @@ func main() {
 		return
 	}
 
-	if Rerr != nil {
-		fmt.Printf("could not read imput \"%v\"\n", input)
-	}
-
 	if Derr != nil {
-		fmt.Printf("could not find directory")
+		fmt.Printf("could not find directory \n")
+		return
 	}
+}
+
+func note(m int, octaves int) uint8 {
+	var (
+		n uint8
+		o int
+	)
+	oct := octaves
+	r := rand.Intn(8)
+	//random number of loops to add octave
+	if octaves > 1 {
+		o = rand.Intn(oct) + 1
+	} else {
+		o = octaves
+	}
+	if m == 0 {
+		switch r {
+		//C
+		case 0:
+			n = 72
+		//D
+		case 1:
+			n = 74
+		//E
+		case 2:
+			n = 76
+		//F
+		case 3:
+			n = 77
+		//G
+		case 4:
+			n = 79
+		//A
+		case 5:
+			n = 81
+		//B
+		case 6:
+			n = 83
+		//C
+		case 7:
+			n = 84
+		}
+	} else {
+		switch r {
+		//C
+		case 0:
+			n = 72
+		//D
+		case 1:
+			n = 74
+		//Eb
+		case 2:
+			n = 75
+		//F
+		case 3:
+			n = 77
+		//G
+		case 4:
+			n = 78
+		//Ab
+		case 5:
+			n = 81
+		//Bb
+		case 6:
+			n = 82
+		//C
+		case 7:
+			n = 84
+		}
+	}
+	for i := 1; i < o; i++ {
+		n += 12
+	}
+	return n
 }
